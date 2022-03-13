82de884cd83812e781e52e03ac390836e2cb13c7
ambri <bachukilabadze@gmail.com>
2022-03-13 19:34:52 +0400

    Module 3: Intro to OOP

---
+++ Object-Oriented Programming/Classes, Objects & Attributes/docs.py
@@ -0,0 +1,34 @@
+""" An object is an incarnation of the requirements, traits, and qualities assigned to a specific class """
+
+
+""" Each subclass is more specialized (or more specific) than its superclass. Conversely, each superclass 
+is more general (more abstract) than any of its subclasses."""
+
+
+""" Inheritance
+Let's define one of the fundamental concepts of object programming, named inheritance. 
+Any object bound to a specific level of a class hierarchy inherits all the traits (as well as the 
+requirements and qualities) defined inside any of the superclasses."""
+
+
+""" every existing object may be equipped with three groups of attributes:
+1)  an object has a name that uniquely identifies it within its home namespace (although there may be some anonymous 
+    objects, too)
+2)  an object has a set of individual properties which make it original, unique or outstanding (although it's possible
+    that some objects may have no properties at all)
+3)  an object has a set of abilities to perform specific activities, able to change the object itself, or some of 
+    the other objects."""
+
+
+""" Whenever you describe an object and you may use:
+
+a noun - you probably define the object's name;
+an adjective - you probably define the object's property;
+a verb - you probably define the object's activity.
+"""
+
+
+"""  A class is an idea (more or less abstract) which can be used to create a number of incarnations – 
+such an incarnation is called an object."""
+
+
